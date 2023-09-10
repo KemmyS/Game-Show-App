b76553ebdb3714d3b2caee004790236f07586ba7
Octuber Sahn <octuberksahn@gmail.com>
2023-09-10 13:00:38 -0500

    Completed step 1-7

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const qwerty = document.getElementById("qwerty");
-const phrase = document.getElementById("phrase");
 
+const phraseContainer = document.getElementById("phrase");
+const phraseUl = phraseContainer.firstElementChild;
+
+const overlay = document.querySelector("#overlay")
 const startGame = document.querySelector(".btn__reset");
-startGame.addEventListener("click", (e) => {});
+startGame.addEventListener("click", (e) => {
+    overlay.style.display = "none"
+});
 
 const missed = 0;
 const phrases = [
@@ -13,38 +18,7 @@ const phrases = [
   "Never judge a book by its cover",
 ];
 
-
-// function getRandomPhraseAsArray(arr) {
-//   let phrase = "";
-
-//   //A random number that's between 0 and array.length
-//   const randomNumber = getRandomInt(arr.length - 1);
-
-//   //i < arr.length == arr.length - 1
-//   //arr[5], but it needs to be arr[4];
-//   //loop through array to access its elements
-//   for (let i = 0; i < arr.length; i++) {
-//     //String === Character[] == Letter[].
-
-//     // Use randomNumber to get a random index, and the random index to
-//     // get a random phrase.
-//     if (randomNumber == i){
-//         //take one phrase from the array, for loop
-//         phrase = arr[i];
-//     }
-    
-//   }
-
-//   //Taking sentences and turning it into letters including spaces, etc
-//   //Turn phrase from string to an array[] of characters, using split()
-//   const phraseArray = phrase.split(" ");
-
-//   console.log(phraseArray);
-
-//   // return character array
-//   return phraseArray;
-// }
-
+// Create a getRandomPhraseAsArray function.
 function getRandomPhraseAsArray(arr) {
     //A random number that's between 0 and array.length - 1
     const randomNumber = Math.floor(Math.random() * (arr.length - 1));
@@ -55,9 +29,65 @@ function getRandomPhraseAsArray(arr) {
   
     //Taking sentences and turning it into letters including spaces, etc
     //Turn phrase from string to an array[] of characters, using split()
-    const phraseArray = phrase.split(" ");
+    const phraseArray = phrase.split("");
 
     // return character array
     return phraseArray;
   }
-getRandomPhraseAsArray(phrases);
+
+// Create an addPhraseToDisplay function 
+function addPhraseToDisplay(arr){
+   
+    // that loops through an array of characters
+    for( let i = 0; i < arr.length; i++){
+        //Create an empty <li></li> using the DOM
+        const listItem = document.createElement("li");
+
+        //Store the array element inside the <li></li> as text
+        listItem.textContent = arr[i].toLowerCase();
+
+        //Character in the array is not a space.
+        if(arr[i] !== " "){
+            //Add the class “letter” to the list item
+            listItem.classList.add("letter")
+        };
+
+        // Add the <li></li> as a child of the <ul></ul> through the DOM
+        phraseUl.append(listItem);
+    }
+
+}
+
+const randomPhrase = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(randomPhrase);
+
+// Create a checkLetter function.
+function checkLetter(btn){
+    // Create variable with a class of “letter”
+    const listItems = phraseUl.childNodes;
+
+//    store the matching letter inside of a variable,
+    let matchingLetter = null;
+  
+  // loop over the letters 
+  for( let i = 0; i < listItems.length; i++ ){
+      const listItem = listItems[i];
+  
+      // If there’s a match,add the “show” class
+      //  to the list item containing that letter, 
+      if(btn.textContent === listItem.textContent ){
+        listItem.classList.add("show")
+  
+      // store the matching letter inside of a variable
+         matchingLetter = btn.textContent
+  
+      }
+    //   return that letter.
+      return matchingLetter;
+  }
+}
+
+  const qButton = qwerty.firstElementChild.firstElementChild;
+  console.log(qButton);
+//   console.log(qButton.textContent);
+  checkLetter(qButton);
